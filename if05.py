15d8a71578d0eccbb73ccaee7816e1411c1f8389
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-04-19 15:32:31 +0500

    5

--- if05.py
+++ if05.py
@@ -6,4 +6,19 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n%10
+    b=n%100//10
+    c=n%1000//100
+    d=n%10000//1000
+    e=n%100000//10000
+    x=a
+    if x<b:
+        x=b
+    if x<c:
+        x=c
+    if x<d:
+        x=d
+    if x<e:
+        x=e
+    return x
+print(main(12745))
